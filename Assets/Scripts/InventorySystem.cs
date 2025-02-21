4a064ed24f90e63601bd37a5409ef9d466b4f51d
幸大 畠山 <230169@jc-21.jp>
2025-02-21 11:20:59 +0900

    アイテムの説明UIを作った。パネルに表示しているが、マウスカーソルにくっつけて表示することもできそうな気がする。

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class InventorySystem : MonoBehaviour
 {
 
+    public GameObject ItemInfoUI;
+
     public static InventorySystem Instance { get; set; }
 
     public GameObject inventoryScreenUI;
--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -78,4 +78,20 @@ public class PlayerState : MonoBehaviour
             currentHealth -= 10;
         }
     }
+
+    public void setHealth(float newHealth)
+    {
+        currentHealth = newHealth;
+    }
+
+    public void setCalories(float newCalories)
+    {
+        currentCalories = newCalories;
+    }
+
+    public void setHydration(float newHydration)
+    {
+        currentHydrationPercent = newHydration;
+    }
+
 }
